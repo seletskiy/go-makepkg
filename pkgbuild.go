3b367c17f41d0db1f344f9b157149c01144e6ade
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-11-06 15:39:13 +0600

    add git clean to package section

--- pkgbuild.go
+++ pkgbuild.go
@@ -66,5 +66,6 @@ package() {
 		install -DT "$filename" "$pkgdir/usr/bin/$(basename $filename)"
 	done{{range .Files}}
 	install -DT -m0755 "$srcdir/{{.Name}}" "$pkgdir/{{.Path}}"{{end}}
+	git clean -fdX
 }
 `))
